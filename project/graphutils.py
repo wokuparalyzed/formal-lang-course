043159586299e035ddb7277c0f2addfbf6f3f6ca
wokuparalyzed <shestakov1228@gmail.com>
2024-09-09 20:38:03 +0300

    feat: add graph_utils.py and tests for it

---
+++ project/graphutils.py
@@ -0,0 +1,33 @@
+import cfpq_data
+from typing import Tuple
+import networkx as nx
+import pydot
+
+
+class GraphData:
+
+    def __init__(self, name):
+        csv_path = cfpq_data.download(name)
+        self.graph = cfpq_data.graph_from_csv(csv_path)
+    
+    @property
+    def nodes(self):
+        return self.graph.number_of_nodes()
+    
+    @property
+    def edges(self):
+        return self.graph.number_of_edges()
+    
+    @property
+    def labels(self):
+        return cfpq_data.get_sorted_labels(self.graph)
+    
+def save_graph_to_file(graph, output_path):
+    dot_data = nx.nx_pydot.to_pydot(graph).to_string()
+    (dot_graph,) = pydot.graph_from_dot_data(dot_data)
+    dot_graph.write(output_path)
+    
+    
+def create_and_save_two_cycles_graph(n, m, labels: Tuple[str, str], output_path):
+        graph = cfpq_data.labeled_two_cycles_graph(n, m, labels=labels)
+        save_graph_to_file(graph=graph, output_path=output_path)
---
+++ tests/test_graphutils.py
@@ -0,0 +1,34 @@
+import pytest
+import os
+from project.graphutils import GraphData, create_and_save_two_cycles_graph, save_graph_to_file
+import cfpq_data
+import networkx as nx
+
+def test_graph_data():
+    graph_name = "travel"  
+    graph_data = GraphData(graph_name)
+
+    assert graph_data.nodes > 0, "nodes count must be more then 0"
+    assert graph_data.edges > 0, "edges count must be more then 0"
+
+    assert isinstance(graph_data.labels, list), "labeles must be in list"
+    assert len(graph_data.labels) > 0, "labels must not be empty"
+
+def test_save_graph_to_file(tmpdir):
+    graph = nx.complete_graph(5)
+    output_path = tmpdir.join("test_graph.dot")
+
+    save_graph_to_file(graph, output_path)
+
+    assert os.path.exists(output_path), "DOT file was not created"
+
+def test_create_and_save_two_cycles_graph(tmpdir):
+    output_path = tmpdir.join("two_cycles.dot")
+
+    create_and_save_two_cycles_graph(3, 4, ("a", "b"), output_path)
+
+    assert os.path.exists(output_path), "File with 2 cycles was not created"
+
+    with open(output_path, 'r') as f:
+        dot_content = f.read()
+        assert 'a' in dot_content and 'b' in dot_content, "labels 'a' and 'b' must be in file"
